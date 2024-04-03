89df0862fc18079a540e69232bd6455387bff9f1
samehsadine <145492561+samehsadine@users.noreply.github.com>
2024-04-03 14:25:33 -0400

    ajout tests unitaires

---
+++ TP2Tests/GestionJeuxTests.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2.Tests
+{
+    [TestClass()]
+    public class GestionJeuxTests
+    {
+        [TestMethod()]
+
+        public void EngagerCombat_AvecDeuxPersonnageEnVie_AuMoinsUnMort()
+        {             
+            // Arrangevar
+
+            Personnage joueur = new Personnage("Joueur", Classe.Guerrier, new List<Sort>(), Arme.MainsNues);
+            Personnage ennemi = new Personnage("Ennemi", Classe.Assassin, new List<Sort>(), Arme.MainsNues);
+            GestionJeux gestionDeJeu = new GestionJeux(joueur, ennemi);
+
+            //Act
+
+            gestionDeJeu.EngagerCombat();
+            bool joueurEstMort = gestionDeJeu.Joueur.EstMort();
+            bool ennemiEstMort = gestionDeJeu.Ennemi.EstMort();
+
+            //Assert
+            Assert.IsTrue(joueurEstMort || ennemiEstMort);
+        }
+        [TestMethod()]
+        public void EngagerCombat_WhenJoueurAndEnnemiAreAlreadyDead_ReturnsFalse()
+        {
+            // Arrange
+         
+            Personnage joueur = new Personnage("Joueur", Classe.Guerrier, new List<Sort>(), Arme.MainsNues);
+            joueur.Stats.PtsVie = 0;
+            Personnage ennemi = new Personnage("Ennemi", Classe.Archer, new List<Sort>(), Arme.MainsNues);
+            ennemi.Stats.PtsVie = 0;
+
+
+            GestionJeux gestionJeux = new GestionJeux(joueur, ennemi);
+
+            // Act
+            bool result = gestionJeux.EngagerCombat();
+
+            // Assert
+            
+            Assert.IsFalse(result);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EngagerCombat_WhenEnnemiIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+          
+            Personnage joueur = new Personnage("Joueur", Classe.Guerrier, new List<Sort>(), Arme.MainsNues);
+
+            GestionJeux gestionJeux = new GestionJeux(joueur, null);
+
+             gestionJeux.EngagerCombat();
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void EngagerCombat_WhenJoueurIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+      
+            Personnage ennemi = new Personnage("Ennemi", Classe.Archer, new List<Sort>(), Arme.MainsNues);
+
+            GestionJeux gestionJeux = new GestionJeux(null, null);
+
+            // Act & Assert
+
+            gestionJeux.EngagerCombat();
+        }
+        [TestMethod()]
+        public void EngagerCombat_WhenJoueurDefeatsEnnemi_ReturnsTrue()
+        {
+            // Arrange
+
+            Personnage joueur = new Personnage("Joueur", Classe.Guerrier, new List<Sort>(), Arme.MainsNues);
+            Personnage ennemi = new Personnage("Ennemi", Classe.Archer, new List<Sort>(), Arme.MainsNues);
+            ennemi.Stats.PtsVie = 1;
+
+
+            GestionJeux gestionJeux = new GestionJeux(joueur, ennemi);
+
+            // Act
+            bool result = gestionJeux.EngagerCombat();
+
+            // Assert
+            Assert.IsTrue(result);
+            Assert.IsFalse(joueur.EstMort());
+            Assert.IsTrue(ennemi.EstMort());
+        }
+        [TestMethod()]
+        public void EngagerCombat_WhenEnnemiDefeatsJoueur_ReturnsFalse()
+        {
+            // Arrange
+         
+            Personnage joueur = new Personnage("Joueur", Classe.Guerrier, new List<Sort>(), Arme.MainsNues);
+            Personnage ennemi = new Personnage("Ennemi", Classe.Archer, new List<Sort>(), Arme.MainsNues);
+            ennemi.Stats.PtsAttaque = 7;
+            joueur.Stats.PtsAttaque=1;
+           
+
+           
+            GestionJeux gestionJeux = new GestionJeux(joueur, ennemi);
+
+            // Act
+            bool result = gestionJeux.EngagerCombat();
+
+            // Assert
+            Assert.IsFalse(result);
+            Assert.IsTrue(joueur.EstMort());
+            Assert.IsFalse(ennemi.EstMort());
+        }
+     
+    }
+       
+}
+
+ 
---
+++ TP2Tests/PersonnageTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TP2.Tests
+{
+    [TestClass()]
+    public class PersonnageTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PersonnageTestNomNull()
+        {
+            Personnage joueur = new Personnage(null, Classe.Archer, new List<Sort>(), Arme.EpeeDeuxMains);
+           
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void PersonnageTestNomVide()
+        {
+            Personnage joueur = new Personnage("", Classe.Archer, new List<Sort>(), Arme.EpeeDeuxMains);
+
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PersonnageTestSortNullPourMage()
+        {
+            Personnage joueur = new Personnage("Sameh", Classe.Mage, null, Arme.EpeeDeuxMains);
+
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PersonnageTestSortPourMage()
+        {
+            Personnage joueur = new Personnage("Sameh", Classe.Mage, new List<Sort>(), Arme.EpeeDeuxMains);
+            Arme Expected = Arme.MainsNues;
+            Assert.AreEqual(Expected, joueur.Arme);
+
+
+        }
+        [TestMethod()]
+        public void AttaquerTest()
+        {
+            Personnage joueur = new Personnage("Sameh", Classe.Archer, new List<Sort>(), Arme.EpeeDeuxMains);
+            Personnage ennemi = new Personnage("Larissa", Classe.Guerrier, new List<Sort>(), Arme.EpeeDeuxMains);
+
+            //Act
+
+            joueur.Attaquer(ennemi);
+            ennemi.Attaquer(joueur);
+
+            //Assert
+            //Assert.AreEqual(joueur.DegatsDernierCombats[0], -ennemi.DegatsDernierCombats[0]);
+            Assert.AreEqual(joueur.DegatsDernierCombats[^1], -ennemi.DegatsDernierCombats[^1]);
+
+        }
+    }
+}
+
